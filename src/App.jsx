8d7fa89b90ada79ecb7fb054fb8f586169f3c023
chrisbrody <chrisbrody@codifyacademy.com>
2022-12-27 15:02:30 -0500

    finished website build

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+import { Brand, Cta, Navbar } from "./components"
+import { Blog, Features, Footer, Header, Possibility, WhatGPT3 } from './containers'
+
+import './App.css'
+
+const app = () => {
+  return (
+    <div className="App">
+        <div className="gradient__bg">
+            <Navbar />
+            <Header />
+        </div>
+        <Brand />
+        <WhatGPT3 />
+        <Features />
+        <Possibility />
+        <Cta />
+        <Blog />
+        <Footer />
+    </div>
+  )
+}
+
+export default app
---
+++ src/components/brand/Brand.jsx
@@ -0,0 +1,24 @@
+import { google, slack, atlassian, dropbox, shopify } from './imports';
+import './brand.css';
+
+const Brand = () => (
+  <div className="gpt3__brand section__padding">
+    <div>
+      <img src={google} alt="Google Logo" />
+    </div>
+    <div>
+      <img src={slack} alt="Slack Logo" />
+    </div>
+    <div>
+      <img src={atlassian} alt="atlassian Logo" />
+    </div>
+    <div>
+      <img src={dropbox} alt="Dropbox Logo" />
+    </div>
+    <div>
+      <img src={shopify} alt="Shopify Logo" />
+    </div>
+  </div>
+);
+
+export default Brand;
